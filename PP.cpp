1366d04c1ad29fcd60f9755085854452156a91e5
Radik Zabirov <zabirovradik070@gmail.com>
2025-02-15 14:39:02 +0400

    changes to lab2

--- PP.cpp
+++ PP.cpp
@@ -6,6 +6,7 @@
 #include <time.h>
 #include <filesystem>
 
+#include <omp.h>
 
 std::chrono::steady_clock::time_point start, end;
 
@@ -68,11 +69,29 @@ public:
 		}
 	}
 
-	void save_to_file(const std::string& filename) const {
+	void save_to_file_lab1(const std::string& filename) const {
 		
-		if(!std::filesystem::exists("../../../matrix&muls/" + std::to_string(data.size())))
-			std::filesystem::create_directories("../../../matrix&muls/" + std::to_string(data.size()));
-		std::ofstream file("../../../matrix&muls/" + std::to_string(data.size()) + "/" + filename + ".txt");
+		if(!std::filesystem::exists("../../../lab1/matrix&muls/" + std::to_string(data.size())))
+			std::filesystem::create_directories("../../../lab1/matrix&muls/" + std::to_string(data.size()));
+		std::ofstream file("../../../lab1/matrix&muls/" + std::to_string(data.size()) + "/" + filename + ".txt");
+		if (file.is_open()) {
+			for (const auto& col : data) {
+				for (const auto& value : col) {
+					file << value << " ";
+				}
+				file << std::endl;
+			}
+			file.close();
+		}
+		else {
+			throw std::invalid_argument("Can't save to file");
+		}
+	}
+	void save_to_file_lab2(const std::string& filename) const {
+
+		if (!std::filesystem::exists("../../../lab2/matrix&muls&results/" + std::to_string(data.size())))
+			std::filesystem::create_directories("../../../lab2/matrix&muls&results/" + std::to_string(data.size()));
+		std::ofstream file("../../../lab2/matrix&muls&results/" + std::to_string(data.size()) + "/" + filename + ".txt");
 		if (file.is_open()) {
 			for (const auto& col : data) {
 				for (const auto& value : col) {
@@ -106,6 +125,35 @@ Matrix multiply(const Matrix& first, const Matrix& second) {
 	return result;
 }
 
+Matrix mul_with_OMP(const Matrix& first, const Matrix& second)
+{
+	start = std::chrono::high_resolution_clock::now();
+	if (first.cols() != second.rows()) {
+		throw std::invalid_argument("The number of columns of matrix A does not match the number of rows of matrix B");
+	}
+	double start = omp_get_wtime();
+	Matrix result(first.rows(), second.cols());
+	int i = 0, j = 0, k = 0;
+	//omp_set_num_threads(12);
+	std::cout << omp_get_num_threads() << " ";
+#pragma omp parallel for shared(first, second, result) private(int i, int j, int k)
+	for (i = 0; i < second.rows(); i++)
+	{
+		for (j = 0; j < first.cols(); j++)
+		{
+			result.at(i, j) = 0;
+			for (k = 0; k < first.cols(); k++)
+			{
+				result.at(i, j) += first.at(i, k) * second.at(k, j);
+			}
+
+		}
+	}
+	std::cout << "time: " << omp_get_wtime() - start << std::endl;
+	end = std::chrono::high_resolution_clock::now();
+	return result;
+}
+
 Matrix load_from_file(const std::string& filename) {
 	std::ifstream file(filename);
 	if (file.is_open()) {
@@ -132,24 +180,23 @@ void experiments_lab1(const std::vector<size_t>& sizes) {
 	for (size_t size : sizes) {
 		Matrix A(size, size, 1);
 		Matrix B(size, size, 1);
-		A.save_to_file("A");
-		B.save_to_file("B");
-		multiply(A, B).save_to_file("multiplyed");
+		A.save_to_file_lab1("A");
+		B.save_to_file_lab1("B");
+		multiply(A, B).save_to_file_lab1("multiplyed");
 		file << "size: " << size << ", time: " << std::chrono::duration_cast<std::chrono::duration<double>>(end - start).count() << std::endl;
 	}
 	file.close();
 }
-
-void experiments_lab1(const std::vector<size_t>& sizes) {
-	std::ofstream file("../../../lab1/results_lab1.txt");
+void experiments_lab2(const std::vector<size_t>& sizes) {
+	std::ofstream file("../../../lab2/results_lab2.txt");
 	if (!file.is_open())
-		throw std::invalid_argument("Can't open file results_lab1.txt");
+		throw std::invalid_argument("Can't open file results_lab2.txt");
 	for (size_t size : sizes) {
 		Matrix A(size, size, 1);
 		Matrix B(size, size, 1);
-		A.save_to_file("A");
-		B.save_to_file("B");
-		multiply(A, B).save_to_file("multiplyed");
+		A.save_to_file_lab2("A");
+		B.save_to_file_lab2("B");
+		mul_with_OMP(A, B).save_to_file_lab2("multiplyed");
 		file << "size: " << size << ", time: " << std::chrono::duration_cast<std::chrono::duration<double>>(end - start).count() << std::endl;
 	}
 	file.close();
@@ -159,6 +206,6 @@ int main()
 {
 	srand(time(NULL));
 	std::vector<size_t> sizes = {10, 100, 200, 500, 800, 1000, 1500, 2000};
-	experiments_lab1(sizes);
+	experiments_lab2(sizes);
 	return 0;
 }
